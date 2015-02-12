4edb0dea77ecbc127f15528d640ce535b9bb5bf1
Chris Russell <chris@azuqua.com>
2015-02-12 12:53:00 -0800

    Update index.js (actually make it terse).

--- index.js
+++ index.js
@@ -1,52 +1,37 @@
 ////
-// Object Namespace Manager (onm) 
-// MIT License // Copyright (C) 2014 Encapsule Project
-// https://github.com/Encapsule/onm/blob/master/LICENSE
+/*
+  Object Namespace Manager (onm)
 
-//
-// A 'data model' represents the schema of a class of JSON documents
-// in terms of addressable, dot-delimited, namespace hierarchy.
-// Typically:
-// var onm = require('onm');
-// var exampleDataModelDeclaration = { jsonTag: 'exampleDataComponent' };
-// var exampleDataModel = new onm(exampleDataModelDeclaration);
+  +-------------+---------------+
+  | onm.Address | onm.Namespace |
+  +-------------+---------------+
+  | onm.Model   | onm.Store     |
+  +-------------+---------------+
+
+  Copyright (C) 2015 Encapsule Project (http://encapsule.org)
+
+  License: MIT (https://github.com/Encapsule/onm/blob/master/LICENSE)
+
+  Source: https://github.com/Encapsule/onm
+
+  Issues: https://github.com/Encapsule/onm/issues
+
+  Designed and implemented by Christopher D. Russell
+
+*/
+
+// onm.Model - schema of a class of hierarchical data objects called namespaces.
 module.exports.Model = require('./lib/onm-model');
 
-//
-// A 'data store' represents a specific instance of a JSON document that
-// conforms to the schema defined by an onm.Model object instance.
-// Typically:
-// var exampleDataStore = new onm.Store(exampleDataModel);
-// console.log(exampleDataStore.toJSON());
-// -> '{ "exampleDataComponent": {} }'
-module.exports.Store = require('./lib/onm-store');
-
-//
-// A 'data address' represents the data model declaration of a specific
-// namespace node in an onm.Model object, and is used to access the specific
-// details of that namespace declaration at runtime. Note, that this is the
-// basis of onm's support for data introsepction.
-// Typically:
-// var rootAddress = exampleDataModel.createRootAddress();
-// console.log(JSON.stringify(rootAddress.getModel());
-// -> "{ 'jsonTag': 'exampleDataComponent' }"
+// onm.Address - schema of a particular namespace in the model's defined address space.
 module.exports.Address = require('./lib/onm-address');
 
-//
-// A 'data namespace' is a proxy object that is used to access JSON document
-// data inside an onm.Store object.
-// Typically:
-// var rootNamespace = exampleDataStore.openNamespace(rootAddress);
-// rootNamespace.data().testProperty = "test";
-// console.log(exampleDataStore.toJSON());
-// -> '{ "exampleDataComponent": { "testProperty": "test" } }'
+// onm.Store - addressable, introspectable, observable, in-memory data cache of namespaces.
+module.exports.Store = require('./lib/onm-store');
+
+// onm.Namespace - address, introspection, and data access proxy object.
 module.exports.Namespace = require('./lib/onm-namespace');
 
-////
-// UTILITY EXPORTS
+// onm.util (test/utility exports)
 module.exports.util = require('./lib/impl/onm-util-functions');
-
-////
-// DEPRECATED in onm v0.3+
-module.exports.AddressStore = require('./lib/onm-address-store');
 
