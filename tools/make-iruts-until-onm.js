5484962425012d4aa3b592ec072d146a9a438db4
Chris Russell <chrisrus@encapsule.org>
2015-03-03 16:46:24 -0800

    Just generate blocks of IRUT's w/out filtering.

--- tools/make-iruts-until-onm.js
+++ tools/make-iruts-until-onm.js
@@ -1,24 +1,10 @@
 RISP = require('../lib/core/risp/risp');
 
-count = 0;
-keepOnTrucking = true
-
-
-while (keepOnTrucking) {
-
+var count = 32;
+while (count-- > 0) {
     var irut = RISP.generate( { format: 'irut' } ).result.value;
-
-    if ((count % 1000000) === 0) {
-        console.log(count + " " + irut + " ... still looking");
-    }
-
-    if (irut.indexOf('onm') === 0) {
-        keepOnTrucking = false;
-    }
-
-    count++
-
+    console.log("'" + irut + "'");
 }
 
-console.log("'" + irut + "' # <= FOUND IRUT beginning in 'onm' in " + count + " attempts.");
+
 
