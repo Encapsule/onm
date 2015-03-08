2d5ad6de3e87670d9db988a23a438e1f86072e61
Chris Russell <chrisrus@encapsule.org>
2015-03-07 22:29:02 -0800

    Update webpack.config.js w/OccurenceOrderPlugin... ... corrects unstable ___webpack_require__ ordinal assignment in generated bundles. Now idempotent w/respect to inputs as far as Git is concerned. Thank you, @sokra for quick reply webpack/webpack#864.

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@
 // https://github.com/petehunt/webpack-howto
 // CRITICAL: https://github.com/webpack/webpack/issues/839
 // http://christianalfoni.github.io/javascript/2014/12/13/did-you-know-webpack-and-react-is-awesome.html
+// https://github.com/webpack/webpack/issues/864#issuecomment-77725887
+
+var webpack = require('webpack');
 
 var outputFilename = 'onm.js'
 
@@ -12,6 +15,9 @@ var fs = require('fs');
 var node_modules = fs.readdirSync('node_modules').filter(function(x) { return x !== '.bin' });
 
 module.exports = {
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin()
+    ],
     entry: {
         main: './index.js'
     },
